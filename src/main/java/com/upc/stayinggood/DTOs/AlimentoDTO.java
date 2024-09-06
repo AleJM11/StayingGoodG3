1a14681c4ad63a100a4a23722fed4d2e4bab3c03
AleJM11 <118072786+AleJM11@users.noreply.github.com>
2024-09-05 22:42:47 -0500

    New files

---
+++ src/main/java/com/upc/stayinggood/DTOs/AlimentoDTO.java
@@ -0,0 +1,24 @@
+package com.upc.stayinggood.DTOs;
+
+import com.upc.stayinggood.Entities.TipoPorcion;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AlimentoDTO {
+    private Integer id;
+    private String nombre;
+    private Integer calorias;
+    private Integer proteinas;
+    private Integer carbohidratos;
+    private Integer grasas;
+    private Double cantidad;
+    private TipoPorcion tipoPorcion;
+}
