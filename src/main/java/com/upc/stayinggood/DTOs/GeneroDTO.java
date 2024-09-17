e539e0a1e39d3b1812ce38e88345e74af6784d19
Jesús Aguilar <u202117213@upc.edu.pe>
2024-09-17 00:20:39 -0500

    prueba_dtoGenero

--- src/main/java/com/upc/stayinggood/DTOs/GeneroDTO.java
+++ src/main/java/com/upc/stayinggood/DTOs/GeneroDTO.java
@@ -1,4 +1,19 @@
 package com.upc.stayinggood.DTOs;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class GeneroDTO {
+    private Integer id;
+    private String nombreGenero;
+    private Integer idGenero;
 }
+
