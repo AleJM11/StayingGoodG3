19ac30e8d98ce6e1f93438704afded0b673790e3
Jesús Aguilar <u202117213@upc.edu.pe>
2024-09-19 23:32:45 -0500

    Security

---
+++ src/main/java/com/upc/stayinggood/Security/JwtRequest.java
@@ -0,0 +1,13 @@
+package com.upc.stayinggood.Security;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class JwtRequest {
+
+}
